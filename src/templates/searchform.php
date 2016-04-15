aeceb1a0c9600c5abb94854a91c78d97332b5a80
Alex Ballera <alexballera@gmail.com>
2016-04-15 14:21:05 -0430

    Mejorado codigo de search

--- src/templates/searchform.php
+++ src/templates/searchform.php
@@ -1,9 +1,9 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
     <label>
-        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+        <span class="screen-reader-text"><?php echo esc_attr_e( 'Search for:', 'portfolio-one' ) ?></span>
         <input type="search" class="search-field"
-            placeholder="<?php echo esc_attr_x( 'Buscar &#46;&#46;&#46;', 'placeholder' ) ?>"
+            placeholder="<?php echo esc_attr_e( 'Search &#46;&#46;&#46;', 'portfolio-one' ) ?>"
             value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+            title="<?php echo esc_attr_e( 'Search for:', 'portfolio-one' ) ?>" />
     </label>
 </form>
