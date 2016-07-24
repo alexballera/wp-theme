b0f1b31a7b6166f87062a79568f06bf458d01265
Alex Ballera <alexballera@gmail.com>
2016-07-24 11:29:55 -0400

    Optimizado scss y fontawesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
-const gulp = require('gulp')
-const browserSync = require('browser-sync')
-const reload = browserSync.reload
-const sass = require('gulp-sass')
-const autoprefixer = require('gulp-autoprefixer')
-const rename = require('gulp-rename')
-const browserify = require('browserify')
-const source = require('vinyl-source-stream')
-const buffer = require('vinyl-buffer')
-const uglify = require('gulp-uglify')
-const babelify = require('babelify')
-const imagemin = require('gulp-imagemin')
-const pngquant = require('imagemin-pngquant')
-const imageminSvgo = require('imagemin-svgo')
-const imageminOptipng = require('imagemin-optipng')
-const imageminJpegtran = require('imagemin-jpegtran')
-const cache = require('gulp-cache')
-const del = require('del')
+var gulp = require('gulp')
+var browserSync = require('browser-sync')
+var reload = browserSync.reload
+var sass = require('gulp-sass')
+var sassGlob = require('gulp-sass-glob')
+var autoprefixer = require('gulp-autoprefixer')
+var rename = require('gulp-rename')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
+var uglify = require('gulp-uglify')
+var babelify = require('babelify')
+var imagemin = require('gulp-imagemin')
+var pngquant = require('imagemin-pngquant')
+var imageminSvgo = require('imagemin-svgo')
+var imageminOptipng = require('imagemin-optipng')
+var imageminJpegtran = require('imagemin-jpegtran')
+var cache = require('gulp-cache')
+var del = require('del')
 // Para que babelify funcione: instalar babel-preset-es2015
 // sudo npm install --save-dev babel-preset-es2015
 
 // Variables
-const globs = {
+var globs = {
   build: './build',
   src: './src',
-  dist: './dist',
+  public: './public',
   php: {
     main: './src/*.php',
     watch: './src/**/*.php'
@@ -32,31 +33,31 @@ const globs = {
     main: './src/styles/scss/style.scss',
     watch: './src/styles/scss/**/*.scss',
     src: './src/styles',
-    dist: './dist/css'
+    public: './public/css'
   },
   scripts: {
     main: './src/js/main.js',
     watch: './src/js/main.js',
     src: './src/js',
-    dist: './dist/js'
+    public: './public/js'
   },
   images: {
     main: './src/images/**',
     watch: './src/images/**/*.*',
     src: './src/images',
-    dist: './dist/images'
+    public: './public/images'
   },
   videos: {
     main: './src/videos/**',
     watch: './src/videos/**/*.*',
     src: './src/videos',
-    dist: './dist/videos'
+    public: './public/videos'
   },
   fonts: {
     main: './src/styles/fonts/**',
     watch: './src/styles/fonts/**/*.*',
     src: './src/styles/fonts',
-    dist: './dist/fonts'
+    public: './public/fonts'
   }
 }
 
@@ -66,7 +67,7 @@ gulp.task('serve', () => {
     notify: false,
     logPrefix: 'BS',
     server: {
-      baseDir: [globs.dist]
+      baseDir: [globs.public]
     },
     port: 8080,
     ui: {
@@ -78,14 +79,15 @@ gulp.task('serve', () => {
 // PHP
 gulp.task('build:php', () => {
   gulp.src(globs.php.watch)
-    .pipe(gulp.dest(globs.dist))
+    .pipe(gulp.dest(globs.public))
 })
 // Styles: Compila SASS ~> CSS
 gulp.task('build:styles', () => {
   return gulp.src(globs.styles.main)
+    .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest(globs.dist))
+    .pipe(gulp.dest(globs.public))
 })
 
 // Scripts: todos los archivos JS concatenados en uno solo minificado
@@ -97,7 +99,7 @@ gulp.task('build:scripts', () => {
     .pipe(buffer())
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(globs.scripts.dist))
+    .pipe(gulp.dest(globs.scripts.public))
 })
 
 // Images
@@ -115,7 +117,7 @@ gulp.task('build:images', ['screenshot'], () => {
         { removeEmptyAttrs: false } // don't remove Empty Attributes from the SVG
       ]
     })))
-    .pipe(gulp.dest(globs.images.dist))
+    .pipe(gulp.dest(globs.images.public))
 })
 gulp.task('screenshot', () => {
   gulp.src(globs.src + '/screenshot.png')
@@ -136,30 +138,30 @@ gulp.task('screenshot', () => {
         { removeEmptyAttrs: false } // don't remove Empty Attributes from the SVG
       ]
     })))
-    .pipe(gulp.dest(globs.dist))
+    .pipe(gulp.dest(globs.public))
 })
 
 // Clean
 gulp.task('clean', (cb) => {
-  return del(globs.dist, cb)
+  return del(globs.public, cb)
 })
 
 // Copy
 gulp.task('copy', () => {
   gulp.src(globs.fonts.src + '/fonts-mfizz/**/*.*')
-    .pipe(gulp.dest(globs.fonts.dist + '/fonts-mfizz'))
-  gulp.src(globs.fonts.src + '/fontawesome/**/*.*') // Comentar si se va a usar el cdnjs
-    .pipe(gulp.dest(globs.fonts.dist + '/fontawesome')) // Comentar si se va a usar el cdnjs
+    .pipe(gulp.dest(globs.fonts.public + '/fonts-mfizz'))
+  gulp.src(globs.fonts.src + '/fonts/**/*.*') // Comentar si se va a usar el cdnjs
+    .pipe(gulp.dest(globs.fonts.public + '/fonts')) // Comentar si se va a usar el cdnjs
   gulp.src(globs.videos.watch)
-    .pipe(gulp.dest(globs.videos.dist))
+    .pipe(gulp.dest(globs.videos.public))
   gulp.src(globs.scripts.src + '/vendors/**/*.*')
-    .pipe(gulp.dest(globs.scripts.dist + '/vendors'))
+    .pipe(gulp.dest(globs.scripts.public + '/vendors'))
   gulp.src(globs.styles.src + '/flexslider.css')
-    .pipe(gulp.dest(globs.styles.dist))
+    .pipe(gulp.dest(globs.styles.public))
   gulp.src(globs.fonts.src + '/fonts-flexslides/**/*.*')
-    .pipe(gulp.dest(globs.styles.dist + '/fonts'))
+    .pipe(gulp.dest(globs.styles.public + '/fonts'))
   gulp.src(globs.src + '/login/**/*.*')
-    .pipe(gulp.dest(globs.dist + '/login'))
+    .pipe(gulp.dest(globs.public + '/login'))
 })
 
 // Reload
