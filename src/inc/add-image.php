8f838f30e7c9cec52919a54e7a3a311654d72c11
Alex Ballera <alexballera@gmail.com>
2016-08-14 15:01:52 -0400

    Mejorado add image

--- src/inc/add-image.php
+++ src/inc/add-image.php
@@ -1,23 +1,22 @@
 <?php 
-/* Convertir la primera im&#225;gen en destacada */
-function autoset_featured() {
+
+function imagen_destacada_automatica() {
     global $post;
-    $already_has_thumb = has_post_thumbnail($post->ID);
-    if (!$already_has_thumb) {
-        $attached_image = get_children( "post_parent=$post->ID&post_type=attachment&post_mime_type=image&numberposts=1" );
+    $ya_tiene_imagen_destacada = has_post_thumbnail($post->ID);
+    if (!$ya_tiene_imagen_destacada)  {
+    $attached_image = get_children( "post_parent=$post->ID&post_type=attachment&post_mime_type=image&numberposts=1" );
         if ($attached_image) {
             foreach ($attached_image as $attachment_id => $attachment) {
                 set_post_thumbnail($post->ID, $attachment_id);
             }
-        }
-    }
-    } //end function
-add_action('the_post', 'autoset_featured');
-add_action('save_post', 'autoset_featured');
-add_action('draft_to_publish', 'autoset_featured');
-add_action('new_to_publish', 'autoset_featured');
-add_action('pending_to_publish', 'autoset_featured');
-add_action('future_to_publish', 'autoset_featured');
-/* Fin convertir la primera im&#225;gen en destacada */
+           }
+     }
+}
+add_action('the_post', 'imagen_destacada_automatica');
+add_action('save_post', 'imagen_destacada_automatica');
+add_action('draft_to_publish', 'imagen_destacada_automatica');
+add_action('new_to_publish', 'imagen_destacada_automatica');
+add_action('pending_to_publish', 'imagen_destacada_automatica');
+add_action('future_to_publish', 'imagen_destacada_automatica');
 
  ?>
