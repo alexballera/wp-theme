3cc7f0794a5df3bf1f8350fbf61400e47658e57e
Alex Ballera <alexballera@gmail.com>
2016-08-18 11:25:05 -0400

    Optimizado login

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
-var gulp = require('gulp')
-var browserSync = require('browser-sync')
-var reload = browserSync.reload
-var sass = require('gulp-sass')
-var sassGlob = require('gulp-sass-glob')
-var autoprefixer = require('gulp-autoprefixer')
-var rename = require('gulp-rename')
-var browserify = require('browserify')
-var source = require('vinyl-source-stream')
-var buffer = require('vinyl-buffer')
-var uglify = require('gulp-uglify')
-var babelify = require('babelify')
-var imagemin = require('gulp-imagemin')
-var pngquant = require('imagemin-pngquant')
-var imageminSvgo = require('imagemin-svgo')
-var imageminOptipng = require('imagemin-optipng')
-var imageminJpegtran = require('imagemin-jpegtran')
-var cache = require('gulp-cache')
-var del = require('del')
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+const reload = browserSync.reload
+const sass = require('gulp-sass')
+const sassGlob = require('gulp-sass-glob')
+const autoprefixer = require('gulp-autoprefixer')
+const cssnano = require('gulp-cssnano')
+const rename = require('gulp-rename')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const uglify = require('gulp-uglify')
+const babelify = require('babelify')
+const imagemin = require('gulp-imagemin')
+const pngquant = require('imagemin-pngquant')
+const imageminSvgo = require('imagemin-svgo')
+const imageminOptipng = require('imagemin-optipng')
+const imageminJpegtran = require('imagemin-jpegtran')
+const cache = require('gulp-cache')
+const del = require('del')
 // Para que babelify funcione: instalar babel-preset-es2015
 // sudo npm install --save-dev babel-preset-es2015
 // sudo npm install --save jquery
 // sudo npm install --save-dev gulp-sass-glob
 
 // Variables
-var globs = {
+const globs = {
   build: './build',
   src: './src',
   public: './public',
@@ -84,12 +85,21 @@ gulp.task('build:php', () => {
     .pipe(gulp.dest(globs.public))
 })
 // Styles: Compila SASS ~> CSS
-gulp.task('build:styles', () => {
+gulp.task('build:styles', ['loginCSS'], () => {
   return gulp.src(globs.styles.main)
     .pipe(sassGlob())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 2 version'))
+    .pipe(cssnano())
     .pipe(gulp.dest(globs.public))
+})
+gulp.task('loginCSS', () => {
+  return gulp.src(globs.src + '/login/custom-login.scss')
+    .pipe(sassGlob())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(cssnano())
+    .pipe(gulp.dest(globs.public + '/login'))
 })
 
 // Scripts: todos los archivos JS concatenados en uno solo minificado
@@ -105,14 +115,19 @@ gulp.task('build:scripts', () => {
 })
 
 // Images
-gulp.task('build:images', ['screenshot'], () => {
+gulp.task('build:images', ['screenshot', 'login'], () => {
   gulp.src(globs.images.main)
     .pipe(cache(imagemin({
       optimizationLevel: 7,
       progressive: true,
       interlaced: true,
       multipass: true,
-      use: [pngquant(), imageminSvgo()],
+      use: [
+        pngquant(),
+        imageminSvgo(),
+        imageminOptipng({optimizationLevel: 7}),
+        imageminJpegtran({progressive: true})
+      ],
       svgoPlugins: [
         { removeViewBox: false }, // don't remove the viewbox atribute from the SVG
         { removeUselessStrokeAndFill: false }, // don't remove Useless Strokes and Fills
@@ -142,6 +157,27 @@ gulp.task('screenshot', () => {
     })))
     .pipe(gulp.dest(globs.public))
 })
+gulp.task('login', () => {
+  gulp.src(globs.src + '/login/*.*g')
+    .pipe(cache(imagemin({
+      optimizationLevel: 7,
+      progressive: true,
+      interlaced: true,
+      multipass: true,
+      use: [
+        pngquant(),
+        imageminSvgo(),
+        imageminOptipng({optimizationLevel: 7}),
+        imageminJpegtran({progressive: true})
+      ],
+      svgoPlugins: [
+        { removeViewBox: false }, // don't remove the viewbox atribute from the SVG
+        { removeUselessStrokeAndFill: false }, // don't remove Useless Strokes and Fills
+        { removeEmptyAttrs: false } // don't remove Empty Attributes from the SVG
+      ]
+    })))
+    .pipe(gulp.dest(globs.public + '/login'))
+})
 
 // Clean
 gulp.task('clean', (cb) => {
@@ -152,18 +188,12 @@ gulp.task('clean', (cb) => {
 gulp.task('copy', () => {
   gulp.src(globs.fonts.src + '/fonts-mfizz/**/*.*')
     .pipe(gulp.dest(globs.fonts.public + '/fonts-mfizz'))
+  gulp.src(globs.fonts.src + '/fonts-flexslides/**/*.*')
+    .pipe(gulp.dest(globs.fonts.public + '/fonts-flexslides'))
   gulp.src(globs.fonts.src + '/fonts/**/*.*') // Comentar si se va a usar el cdnjs
     .pipe(gulp.dest(globs.fonts.public + '/fonts')) // Comentar si se va a usar el cdnjs
   gulp.src(globs.videos.watch)
     .pipe(gulp.dest(globs.videos.public))
-  gulp.src(globs.scripts.src + '/vendors/**/*.*')
-    .pipe(gulp.dest(globs.scripts.public + '/vendors'))
-  gulp.src(globs.styles.src + '/flexslider.css')
-    .pipe(gulp.dest(globs.styles.public))
-  gulp.src(globs.fonts.src + '/fonts-flexslides/**/*.*')
-    .pipe(gulp.dest(globs.styles.public + '/fonts'))
-  gulp.src(globs.src + '/login/**/*.*')
-    .pipe(gulp.dest(globs.public + '/login'))
 })
 
 // Reload
@@ -191,3 +221,4 @@ gulp.task('build', ['clean'], () => {
 gulp.task('default', ['build'], () => {
   gulp.start('copy', 'watch')
 })
+
