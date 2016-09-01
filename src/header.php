018b0ce0711b9441ac9833d85a930fbff3e92b92
Alex Ballera <alexballera@gmail.com>
2016-09-01 12:12:29 -0400

    Agregado google analytics

--- src/header.php
+++ src/header.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,6 +28,10 @@
   <!-- End injection wp_head -->
   </head>
   <body <?php body_class('body'); ?>>
+
+    <!-- Google Analytics -->
+    <?php include_once('templates/analyticstracking.php') ?>
+    <!-- End Google Analytics -->
 
     <!-- Browsehappy -->
     <?php get_template_part( 'templates/browsehappy'); ?>
